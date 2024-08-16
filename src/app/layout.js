df2442d38ad96cc1807f768a2e4c844b9f4d1fd5
balgovidr <balgovidr@hotmail.com>
2024-08-16 18:07:43 +0100

    Temporarily disabling the feedback icon

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,11 +15,11 @@ export default async function RootLayout({ children }) {
       <body className={inter.className} suppressHydrationWarning={true}>
         <Navbar />
         {children}
-        <Tooltip title="Got any feedback for us?">
+        {/* <Tooltip title="Got any feedback for us?">
           <Fab color="#652cb3" className="hidden md:block fixed bottom-6 right-10 bg-primary text-white content-center text-center hover:text-primary" href="/feedback">
             <FeedbackIcon />
           </Fab>
-        </Tooltip>
+        </Tooltip> */}
       </body>
     </html>
   )
