952a35a3ae538bfc7516e469a60868eccc52208e
balgovidr <balgovidr@hotmail.com>
2024-08-17 15:31:05 +0100

    Google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import './globals.css'
 import Navbar from '@/components/navbar';
 import { Fab, Tooltip } from '@mui/material';
 import FeedbackIcon from '@mui/icons-material/Feedback';
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,15 +12,6 @@ export default async function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link rel="icon" type="image/x-icon" href="/favicon.png"/>
-    		<!-- Google tag (gtag.js) -->
-				<script async src="https://www.googletagmanager.com/gtag/js?id=G-4QXX33B0FZ"></script>
-				<script>
-  				window.dataLayer = window.dataLayer || [];
-  				function gtag(){dataLayer.push(arguments);}
-  				gtag('js', new Date());
-
-  				gtag('config', 'G-4QXX33B0FZ');
-				</script>
       </head>
       <body className={inter.className} suppressHydrationWarning={true}>
         <Navbar />
@@ -30,6 +22,7 @@ export default async function RootLayout({ children }) {
           </Fab>
         </Tooltip> */}
       </body>
+      <GoogleAnalytics gaId="G-4QXX33B0FZ" />
     </html>
   )
 }
