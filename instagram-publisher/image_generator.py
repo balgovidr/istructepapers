b7863f51936147296090d08a1a754970bf81f7f7
balgovidr <balgovidr@hotmail.com>
2024-10-13 23:06:17 +0100

    Initial commit

---
+++ instagram-publisher/image_generator.py
@@ -0,0 +1,105 @@
+from PIL import Image, ImageDraw, ImageFont
+import textwrap
+
+def generate_image_from_text(text, background_color, text_color, img_width, img_height):
+    """Generates a 1080x1080 pixel image with the specified text.
+
+    Args:
+        text: The text to be displayed on the image.
+        background_color: The background color of the image (RGB tuple).
+        text_color: The color of the text (RGB tuple).
+        font_path: The path to the font file to be used.
+        output_path: The path where the output image will be saved.
+    """
+    # Select font file
+    font_path = r'fonts\coolvetica rg.otf'
+
+    # Create a new image
+    img = Image.new('RGB', (img_width, img_height), background_color)
+
+    # Draw the text onto the image
+    draw = ImageDraw.Draw(img)
+    font_size = 80
+
+    # Wrap text to fit within the image width
+    max_width = img_width * 0.9  # Slightly less than img_width to account for padding
+    max_height = img_height * 0.9
+    text_block_height = max_height + 1
+
+    while text_block_height > max_height:
+      font = ImageFont.truetype(font_path, font_size)
+      
+      # Calculate the average character width
+      average_char_width = draw.textlength("J", font=font)  # Width of an average character
+      max_chars_per_line = max_width // average_char_width
+
+      # Wrap text based on the number of characters that fit in the max_width
+      wrapped_text = textwrap.fill(text, width=max_chars_per_line)
+
+      # Split wrapped text into lines
+      lines = wrapped_text.splitlines()
+      line_spacing = 10  # Space between lines
+
+      # Measure the height of the text block to vertically center it
+      line_heights = [draw.textbbox((0, 0), line, font=font)[3] for line in lines]
+      text_block_height = sum(line_heights) + line_spacing * (len(lines) - 1)
+      y_offset = (1080 - text_block_height) // 2
+
+      # Ensure that the text block will fit in the image if not reduce the font size
+      if text_block_height > max_height:
+          font_size -= 1
+
+    # Draw each line centered horizontally
+    for line, line_height in zip(lines, line_heights):
+        line_width = draw.textbbox((0, 0), line, font=font)[2]
+        x = (1080 - line_width) // 2  # Define x within the loop for each line
+        draw.text((x, y_offset), line, fill=text_color, font=font)
+        y_offset += line_height + line_spacing
+
+    # Save the image
+    img.save('output.jpg')
+
+def generate_image_from_image(image_path, img_width, img_height):
+  image = Image.open(image_path, 'r')
+  image_size = image.size
+  width = image_size[0]
+  height = image_size[1]
+
+  aspect_ratio = width/height
+
+  # Resize image
+  if width > img_width | height > img_height:
+    if aspect_ratio > img_width / img_height:
+      # Width of the image is higher than the img_width requirement.
+      image = image.resize((int(img_width), int(img_width / aspect_ratio)))
+    else:
+      # Height of the image is higher than the img_height requirement.
+      image = image.resize((int(img_height * aspect_ratio), int(img_height)))
+
+  image_size = image.size
+  width = image_size[0]
+  height = image_size[1]
+
+  # Provide a white background to the image to make it square
+  if(width != height):
+    bigside = width if width > height else height
+
+    background = Image.new('RGBA', (bigside, bigside), (255, 255, 255, 255))
+    offset = (int(round(((bigside - width) / 2), 0)), int(round(((bigside - height) / 2),0)))
+
+    background.paste(image, offset)
+    background.save('output.jpg')
+    print("Image has been resized !")
+
+  else:
+    print("Image is already a square, it has not been resized !")
+      
+# Example usage
+text = "Extremely long text that I don't think is going to fit on the screen but I'm going to keep writing to make sure it doesn't. Extremely long text that I don't think is going to fit on the screen but I'm going to keep writing to make sure it doesn't."
+background_color = (255, 255, 255)  # White
+text_color = (0, 0, 0)  # Black
+output_path = "output.jpg"
+
+if __name__ == "__main__":
+    # generate_image_from_text(text, background_color, text_color, output_path, 1080, 1080)
+    generate_image_from_image(1080, 1080)
---
+++ instagram-publisher/structural_papers.py
@@ -0,0 +1,90 @@
+import random
+import csv
+from image_generator import *
+
+def random_line_from_csv(filename):
+  """Selects a random line from a CSV file, excluding the header.
+
+  Args:
+    filename: The name of the CSV file.
+
+  Returns:
+    A list representing the selected row.
+  """
+
+  with open(filename, 'r') as csvfile:
+    reader = csv.reader(csvfile)
+    # Skip the header row
+    next(reader)
+    # Get a list of all rows
+    rows = list(reader)
+    # Select a random row
+    random_row = random.choice(rows)
+    return random_row
+
+def structural_papers():
+    # Select if the post is going to be text or an image
+    random_integer = random.randint(1, 3)
+    purple = (101, 44, 179)
+    white = (255, 255, 255)
+
+    if random_integer > 1:
+        # Post a text
+        # Select a random row from the text_posts.csv
+        post = random_line_from_csv('text_posts.csv')
+        content = post[1]
+        caption = post[2]
+
+        # Randomly pick the text and background colours
+        random_integer = random.randint(1, 2)
+
+        if random_integer == 1:
+           # Use a purple background
+           background_color = purple
+           text_color = white
+        else:
+           # Use a white background
+           background_color = white
+           text_color = purple
+
+        # Generate the image
+        generate_image_from_text(content, background_color, text_color, 1080, 1080)
+    
+    else:
+       # Generate an image using one of the images
+       # Select a random row from the image_posts.csv
+        post = random_line_from_csv('image_posts.csv')
+        image = post[0]
+        caption = post[2]
+
+        generate_image_from_image(f'images\\{image}', 1080, 1080)
+    
+    # Modify the caption
+    # Include the 'link in the bio' mention
+    split_text = caption.split('#')
+    message = split_text[0]
+    split_text = caption.split(message)
+    hashtags = split_text[1]
+
+    # Add a space to the end of the message
+    if message[-1] != ' ':
+       message = message + ' '
+    
+    message = message + 'Check link in bio. '
+    
+    #Make sure that the structural engineering tag is in the list of hashtags
+    exists = False
+    for hashtag in hashtags.split('#'):
+       if "structuralengineering" in hashtag or "structuralengineering " in hashtag:
+          exists = True
+    if exists == False:
+       hashtags = hashtags + ' #structuralengineering'
+    
+    # Append the message and hashtag back together
+    caption = message + hashtags
+
+    print(caption)
+    return caption
+
+if __name__ == "__main__":
+   structural_papers()
