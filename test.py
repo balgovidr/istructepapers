ac7c9d7f175fb0f8fda1b56dc13cc49e4d547a6d
balgovidr <balgovidr@hotmail.com>
2024-12-08 12:22:49 +0000

    Python

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import os
 
 def test():
     INSTAGRAM_USERNAME = os.environ.get('INSTAGRAM_USERNAME')
-    print(INSTAGRAM_USERNAME)
+    print("Something " + INSTAGRAM_USERNAME)
 
 if __name__ == "__main__":
     test()
